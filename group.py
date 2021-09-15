ae8d80492279fdab09558a390a985426f410b049
Taylor Powell <taylor.powell2001@gmail.com>
2021-09-15 14:52:01 -0600

    Home and Fact

--- group.py
+++ group.py
@@ -5,6 +5,8 @@ def taylorInfo():
     year = "Junior"
     major = "Computer Science"
     hobbies = "Violin, Music, Hiking, Games :)"
+    home = "Kalispell, MT"
+    fact = "I like dogs :)"
 
 
 def kavinInfo():
